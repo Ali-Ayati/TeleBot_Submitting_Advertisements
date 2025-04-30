a70a5bca0d78bb85eabd8de3fd168a81583a08f6
ali ayati <ali.ayt.ayati@gmail.com>
2025-04-30 19:50:01 +0330

    add message_handler for 'strt' command

--- main.py
+++ main.py
@@ -1,5 +1,29 @@
+from config import *
+from telebot import TeleBot
+import mysql.connector
+
+bot = TeleBot(telgram_api)
+
+@bot.message_handler(commands=['start'])
+def start_command(m):
+    try:
+        with mysql.connector.connect(**db_config) as connection:
+            with connection.cursor() as cursor:
+                sql = "INSERT INTO users (id) VALUES (%s)"
+                val = m.from_user.id
+                cursor.execute(sql, val)
+                connection.commit()
+        bot.send_message(m.chat.id, text="سلام کاربر جدید.")      
+          
+    except:
+        bot.send_message(m.chat.id, text= "سلام کاربر قدیمی.")
+        
+        
+        
+        
+        
 
 
 
 
-
+bot.infinity_polling()
